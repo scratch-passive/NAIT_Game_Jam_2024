74cdcde5e0982571c300b51831c5d47b2f1170e9
scratch-passive <isond@mymacewan.ca>
2024-03-03 08:54:15 -0700

    removed stairs from tilemap

---
+++ Assets/Sprites/Green_Slime/EnemyData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = ScriptableObjects/Enemy, order = 1)]
+
+public class EnemyData : ScriptableObject
+{
+    // Start is called before the first frame update
+    public int hp;
+
+
+}
