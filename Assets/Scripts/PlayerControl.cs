750f078082de942c1ad74c75614a2f32dc853683
Jesse Emery <emeryj6@mymacewan.ca>
2024-03-02 21:55:42 -0700

    Player is moving, starting shooting

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Vector2 moveVal = new(0, 0);
+    public float moveSpeed = 1;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // only move if the input isn't 0
+        if(moveVal.x != 0 || moveVal.y != 0)
+        {
+            transform.Translate(moveSpeed * Time.deltaTime * new Vector3(1, 0, 0));
+        }
+    }
+
+    // called from the player input component "send messages" whenever the player presses a movement key
+    void OnMove(InputValue value)
+    {
+        moveVal = value.Get<Vector2>();
+        //Debug.Log(moveVal);
+        // convert the direction to an angle to move
+
+        if(moveVal.x != 0 || moveVal.y != 0)
+        {
+            float rotation_angle = Mathf.Atan2(moveVal.y, moveVal.x) * Mathf.Rad2Deg;
+            Debug.Log("Rotation angle: " + rotation_angle + ", Right: " + transform.right + ", Up: " + transform.up);
+            transform.rotation = Quaternion.Euler(0, 0, rotation_angle);
+            //anim.Play("Move");
+            // starts the moving animation
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            // stops the moving animation
+            anim.SetBool("IsMoving", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnShoot()
+    {
+        // shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
